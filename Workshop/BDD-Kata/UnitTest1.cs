d26269764cda24b9020b5c0eab1effdf7b5fe232
Andy Lin <andy.lin@egk-tech.com>
2022-06-13 21:21:56 +0800

    init

---
+++ Workshop/BDD-Kata/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace BDD_Kata;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
